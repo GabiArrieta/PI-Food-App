101f427a988235eab58b142ac6f599de8f718d3b
gabiarrieta <gabriela.arrieta.187@gmail.com>
2021-07-29 01:56:04 -0300

    fix routes

--- api/src/models/Diet.js
+++ api/src/models/Diet.js
@@ -1,10 +1,15 @@
-const { DataTypes } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("diet", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
     title: {
       type: DataTypes.STRING,
     },
